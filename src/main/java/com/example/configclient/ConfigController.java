b7a330d4fcc31ae70469a6f394a6feaf6490fcbd
rocday <sjp@xinlonghang.cn>
2017-09-13 13:26:31 +0800

    Initial commit

---
+++ src/main/java/com/example/configclient/ConfigController.java
@@ -0,0 +1,24 @@
+package com.example.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigController {
+    @Value("${info.profile}")
+    private String profile;
+
+    @Value("${info.from}")
+    private String from;
+
+    @Value("${test}")
+    private String test;
+
+    @RequestMapping("/configInfo")
+    String hello() {
+        return "info.profile: " + profile + " info.from: " + from + " test: " + test + " !";
+    }
+}
